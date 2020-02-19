91b5fa0f40af659a7dc6b881add68ad75a5bd428
Stacy Johns <stacykjohns@icloud.com>
2020-02-19 19:12:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,4 +6,16 @@ class Author
     @name = name
   end
 
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 end
